e26acf9d724b57c8af7c54d33e06f9171e91060e
Albert Papp <albert.papp@blacksalt.io>
2018-03-21 17:57:04 +0100

    Some tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Nativescript Webview test
 
+https://github.com/mxth/NativeScript/commit/d7ae10566ef42f32571f2e691be6bb90f75d6521
+
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import { WebView, LoadEventData } from "ui/web-view";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
 
-export class AppComponent { }
+export class AppComponent implements AfterViewInit {
+    @ViewChild('webView') webViewRef: ElementRef;
+
+    onTap(args) {
+        console.log("Tap");
+    }
+
+    ngAfterViewInit() {
+        let webview: WebView = this.webViewRef.nativeElement;
+
+
+
+
+        webview.on(WebView.loadFinishedEvent, function (args: LoadEventData) {
+            let message;
+            if (!args.error) {
+                message = "WebView finished loading of " + args.url;
+
+                if (webview.ios) {
+
+                    webview.ios.scrollView.minimumZoomScale = 1.0;
+                    webview.ios.scrollView.maximumZoomScale = 1.0;
+                    webview.ios.scrollView.zoomScale= 1.0;
+                    webview.ios.scalesPageToFit = false;
+
+                } else if (webview.android) {
+                    webview.android.getSettings().setBuiltInZoomControls(false);
+                }
+            } else {
+                message = "Error loading " + args.url + ": " + args.error;
+            }
+
+
+            console.log("WebView message - " + message);
+        });
+    }
+}
