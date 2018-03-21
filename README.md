d7152ac5a4f63e31109e0b8ade34867f7b2df7b1
Albert Papp <albert.papp@blacksalt.io>
2018-03-21 22:29:47 +0100

    More Tests

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Nativescript Webview test
 
 https://github.com/mxth/NativeScript/commit/d7ae10566ef42f32571f2e691be6bb90f75d6521
+https://www.npmjs.com/package/webviews
 
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
 import { WebView, LoadEventData } from "ui/web-view";
+import { isIOS, isAndroid } from "platform";
+import { Color } from "color";
 
 @Component({
     selector: "ns-app",
@@ -24,15 +26,23 @@ export class AppComponent implements AfterViewInit {
             if (!args.error) {
                 message = "WebView finished loading of " + args.url;
 
-                if (webview.ios) {
+                if (isIOS) {
 
+                    let translucent = new Color("#00ff0000");
+                    webview.ios.backgroundColor = translucent.ios;
+                    webview.ios.opaque = false;
+
+                    webview.ios.scrollView.bounces = false;
                     webview.ios.scrollView.minimumZoomScale = 1.0;
                     webview.ios.scrollView.maximumZoomScale = 1.0;
                     webview.ios.scrollView.zoomScale= 1.0;
                     webview.ios.scalesPageToFit = false;
 
-                } else if (webview.android) {
-                    webview.android.getSettings().setBuiltInZoomControls(false);
+                } else if (isAndroid) {
+                    webview.android.setBackgroundColor(0x00000000);
+               //     webview.android.setLayerType(webview.android.view.View.LAYER_TYPE_SOFTWARE, null);
+                    webview.android.getSettings().setDisplayZoomControls(false);
+
                 }
             } else {
                 message = "Error loading " + args.url + ": " + args.error;
